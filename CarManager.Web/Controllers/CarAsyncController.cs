fcd76d85aabd3c109b4c062221f841acf0ff41ec
Ali Kheyrollahi <aliostad@gmail.com>
2012-09-17 16:27:12 +0100

    Adding Async Car controller

---
+++ CarManager.Web/Controllers/CarAsyncController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using CarManager.Data;
+
+namespace CarManager.Web.Controllers
+{
+	public class CarAsyncController : ApiController
+	{
+		private static readonly ICarRepository _carRepository = new InMemoryCarRepository();
+
+		public IEnumerable<Car> Get()
+		{
+			return _carRepository.GetAll();
+		}
+
+		public Task<Car> GetAsync(int id)
+		{
+			Debug.WriteLine(HttpContext.Current.Request.Url);
+			return Task.Factory.StartNew(() => _carRepository.Get(id));
+		}
+
+		public void Put(Car car)
+		{
+			_carRepository.Update(car);
+		}
+
+		public void Delete(int id)
+		{
+			_carRepository.Delete(id);
+		}
+
+		public HttpResponseMessage Post(Car car)
+		{
+			_carRepository.Add(car);
+			var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.Created);
+			httpResponseMessage.Headers.Location = new Uri(Request.RequestUri.ToString()
+				+ "/" + car.Id.ToString());
+			return httpResponseMessage;
+		}
+
+	}
+}
